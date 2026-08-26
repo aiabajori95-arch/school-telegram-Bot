bee47042b66990246e9f5066e168b2099698ccf7
aiabajori95-arch <aiabajori95@gmail.com>
2026-08-26 18:48:23 +0200

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,22 @@
+import os
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        "🏫 أهلاً بك في بوت مدارس رواد تاج الأصفـياء 💙\n\n"
+        "اختر من القائمة التي سنضيفها قريبًا."
+    )
+
+def main():
+    token = os.getenv("BOT_TOKEN")
+
+    app = Application.builder().token(token).build()
+
+    app.add_handler(CommandHandler("start", start))
+
+    print("Bot is running...")
+    app.run_polling()
+
+if __name__ == "__main__":
+    main()
