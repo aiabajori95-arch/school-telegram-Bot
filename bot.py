21ecf9fd42ec013aae36d1540e7fcfd7a3f0976b
aiabajori95-arch <aiabajori95@gmail.com>
2026-08-26 22:50:32 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,14 +1,25 @@
 import os
 from telegram import Update
-from telegram.ext import Application, CommandHandler, ContextTypes
+from telegram.ext import Application, CommandHandler
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+async def start(update: Update, context):
     await update.message.reply_text("اهلا بك في بوت المدرسة")
 
 def main():
     token = os.getenv("BOT_TOKEN")
-    app = Application.builder().token(token).build()
+
+    app = (
+        Application.builder()
+        .token(token)
+        .connect_timeout(60)
+        .read_timeout(60)
+        .write_timeout(60)
+        .pool_timeout(60)
+        .build()
+    )
+
     app.add_handler(CommandHandler("start", start))
+
     print("Bot is running")
     app.run_polling()
 
